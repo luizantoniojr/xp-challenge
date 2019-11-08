4ba2636e87aea7e3fe6b5dfd759928fa9992b1ab
luizantoniojr <luizantoniosilvajr@outlook.com>
2019-11-08 13:40:15 -0300

    Foi iniciada a configuração do projeto.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import { Provider } from 'react-redux';
+//import Routes from './routes';
+//import store from './store';
+
+import './styles.css';
+
+const App = () => (
+    <div className="App">
+        <h1>TESTE</h1>
+    </div>
+);
+
+export default App;
